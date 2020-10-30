b9d8c5ebd9327acec0c2e9fe7e8cf7665ee11616
Luis Vargas <lewiis29@gmail.com>
2020-10-30 11:18:46 -0600

    Comunicación entre Componentes: Propiedades

--- curso-basico-vuejs-master/app.js
+++ curso-basico-vuejs-master/app.js
@@ -1,58 +1,102 @@
+Vue.component('CoinDetail', {
+    props: ['coin'],
+
+    data() {
+        return {
+            showPrices: false,
+            value: 0
+        }
+    },
+
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.coin.name} - ${this.coin.symbol}`
+        },
+
+        convertedValue() {
+            if (!this.value) {
+                return 0
+            }
+
+            return this.value / this.coin.price
+        }
+    },
+
+    template: `
+<div>
+        <img
+            v-on:mouseover="toggleShowPrices"
+            v-on:mouseout="toggleShowPrices"
+            v-bind:src="coin.img" v-bind:alt="coin.name">
+        <h1
+            v-bind:class="coin.changePercent > 0 ? 'green': 'red'">
+        {{ title }}
+        <span v-if="coin.changePercent > 0"> 👍 </span>
+        <span v-else-if="coin.changePercent < 0"> (☞ﾟヮﾟ)☞ </span>
+        <span v-else> ✌✌ </span>
+
+        <span v-on:click="toggleShowPrices">
+              {{ showPrices ? '👨🏿' : '‍🦰'}}</span>
+        </h1>
+        
+        <input type="number" v-model="value">
+        <span>{{ convertedValue }}</span>
+        
+        <ul v-show=showPrices>
+            <li
+                class="uppercase"
+                v-bind:class="{ orange: p.value === coin.price, red: p.value < coin.price, green: p.value > coin.price }"
+                v-for="(p, i) in coin.pricesWithDays"
+                v-bind:key="p.day">
+                {{ i }} - {{p.day}} - {{ p.value }}
+            </li>
+    </ul>
+</div>
+`
+})
+
+
 new Vue({
     el: '#app',
     data() {
         return {
-            name: 'Bitcoin',
-            symbol:'BTC',
-            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: -10,
+            btc: {
+                name: 'Bitcoin',
+                symbol: 'BTC',
+                img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+                changePercent: -10,
+                price: 8400,
+                pricesWithDays: [
+                    {day: 'Lunes', value: 8400},
+                    {day: 'Martes', value: 7900},
+                    {day: 'Miercoles', value: 8200},
+                    {day: 'Jueves', value: 9000},
+                    {day: 'Viernes', value: 9400},
+                    {day: 'Sabado', value: 10000},
+                    {day: 'Domingo', value: 10200},
+                ],
+            },
 
-            value: 0,
 
             color: 'f4f4f4',
 
-            price: 8400,
-
-            pricesWithDays: [
-                {day: 'Lunes', value: 8400},
-                {day: 'Martes', value: 7900},
-                {day: 'Miercoles', value: 8200},
-                {day: 'Jueves', value: 9000},
-                {day: 'Viernes', value: 9400},
-                {day: 'Sabado', value: 10000},
-                {day: 'Domingo', value: 10200},
-            ],
-
-            showPrices: false
         }
     },
 
-  computed: {
-      title () {
-        return `${this.name} - ${this.symbol}`
-      },
-      convertedValue () {
-          if(!this.value) {
-              return 0
-          }
 
-          return this.value / this.price
-      }
-  },
-
-  watch: {
-    showPrices (newVal, oldVal) {
-        console.log(newVal, oldVal)
-    }
-  },
-
-    methods: {
-        toggleShowPrices() {
-          this.showPrices = !this.showPrices
-
-          this.color = this.color.split('')
-              .reverse().join('')
-        }
-    }
+    // methods: {
+    //     toggleShowPrices() {
+    //         this.showPrices = !this.showPrices
+    //
+    //         this.color = this.color.split('')
+    //             .reverse().join('')
+    //     }
+    // }
 
 })
