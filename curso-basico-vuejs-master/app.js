4241e97584b81814d036815e80f86566147aaf03
Luis Vargas <lewiis29@gmail.com>
2020-10-30 11:37:03 -0600

    Comunicación entre Componentes: Eventos

--- curso-basico-vuejs-master/app.js
+++ curso-basico-vuejs-master/app.js
@@ -11,6 +11,9 @@ Vue.component('CoinDetail', {
     methods: {
         toggleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.$emit('change-color',
+                this.showPrices ? 'FF96C8' : '3D3D3D')
         }
     },
 
@@ -90,13 +93,13 @@ new Vue({
     },
 
 
-    // methods: {
-    //     toggleShowPrices() {
-    //         this.showPrices = !this.showPrices
-    //
-    //         this.color = this.color.split('')
-    //             .reverse().join('')
-    //     }
-    // }
+    methods: {
+        updateColor(color) {
+            this.color = color || this.color
+                .split('')
+                .reverse()
+                .join('')
+        }
+    }
 
 })
