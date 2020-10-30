fb35feb48bff8d881747c204f6a15bea8ff5e763
Luis Vargas <lewiis29@gmail.com>
2020-10-30 08:56:59 -0600

    Two-Way Data Binding

--- curso-basico-vuejs-master/app.js
+++ curso-basico-vuejs-master/app.js
@@ -7,6 +7,8 @@ new Vue({
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: -10,
 
+            value: 0,
+
             color: 'f4f4f4',
 
             price: 8400,
@@ -28,6 +30,13 @@ new Vue({
   computed: {
       title () {
         return `${this.name} - ${this.symbol}`
+      },
+      convertedValue () {
+          if(!this.value) {
+              return 0
+          }
+
+          return this.value / this.price
       }
   },
 
