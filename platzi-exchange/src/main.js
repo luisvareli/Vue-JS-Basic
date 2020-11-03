e0f7ff2fe1b2a9197057d96c8d87325fa100fd55
Luis Vargas <lewiis29@gmail.com>
2020-11-03 16:02:57 -0600

    Mejorar la UI con filtros

--- platzi-exchange/src/main.js
+++ platzi-exchange/src/main.js
@@ -4,7 +4,10 @@ import "@/assets/css/tailwind.css"
 
 
 import router from '@/router'
+import { dollarFilter , percentFilter } from '@/filters'
 
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false
 
 new Vue({
